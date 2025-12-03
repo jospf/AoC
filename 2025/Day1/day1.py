19f1e0b887e800fd67ce7955c9647fa72e882ff2
Jospf <jospf@sdf.org>
2025-12-03 14:54:11 -0500

    day1 part one

--- 2025/Day1/day1.py
+++ 2025/Day1/day1.py
@@ -1,5 +1,17 @@
 # Read File
+zeros = 0
+combo = 50
 
-# Loop through file, reading each line
+with open('input.txt', 'r') as file:
+    for line in file:      # Loop through file, reading each line
+        if line[0] == 'L':
+            combo = (combo - int(line[1:])) % 100
+            if combo == 0:
+                zeros += 1
+        else:
+            combo = (combo + int(line[1:])) % 100
+            if combo == 0:
+                zeros += 1
+print(zeros)
 
 # Count 0
